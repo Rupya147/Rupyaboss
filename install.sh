76dd2db74787855ce52f72bb121544f19ecb0756
Rupya147 <rupyakaddos@gmail.com>
2024-09-23 12:43:28 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ cat <<EOL > .devcontainer/devcontainer.json
 {
     "name": "My Codespace",
     "image": "mcr.microsoft.com/vscode/devcontainers/python:3.8",
-    "postStartCommand": "python3 /workspaces/TrialRupya/m.py",
+    "postStartCommand": "python3 /workspaces/Rupyaboss/m.py",
     "customizations": {
         "vscode": {
             "settings": {
